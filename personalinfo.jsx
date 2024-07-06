588e36bd1d0b6a42b5cf0b914a1dbe65027fb2e9
Shyla Khemraj <shylakhemraj@usf.edu>
2024-07-05 23:10:11 -0400

    finalize.

---
+++ personalinfo.jsx
@@ -0,0 +1,14 @@
+// U20026580
+import React from 'react';
+import './PersonalInfo.css';
+const PersonalInfo = () => {
+  return (
+    <div className="personal-info">
+      <h1>Shyla Khemraj</h1>
+      <p>I am from Port St. Lucie, FL and have been living in Tampa for the past two years. I am going into my senior year at the University of South Florida. A fun fact about me is that I have been to 25 out of the 50 states.</p>
+      <p>Email: shylakhemraj@usf.edu </p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/skhemraj">My LinkedIn</a></p>
+    </div>
+  );
+};
+export default PersonalInfo;
